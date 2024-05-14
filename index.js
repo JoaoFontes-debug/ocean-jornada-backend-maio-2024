4a7a9ddd1d19665d8512202299a7de146ccdab90
João Fontes <j.henry.kings@gmail.com>
2024-05-14 17:30:44 +0000

    read by id

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
 })
 
+// lista de itens
+
+const itens =  ['rick sanchez', 'morty', 'sumer']
+//              0                 1         2
+//endpoint de read All [GET] /item
+app.get('/item', function (req,res) {
+  res.send (itens)
+}) 
+
+//endpoint read by id [GET] /item/:id
+app.get('/item/:id', function(req, res){
+  // acessamos o parametro de rota id
+  const id = req.params.id
+
+  //acessamos o item na lista usando o "id-1"
+  const item = itens[id-1]
+  
+  res.send(item)
+})
+
+//parametros de rotas exemplo "/:id"
+
+
 app.listen(3000)
